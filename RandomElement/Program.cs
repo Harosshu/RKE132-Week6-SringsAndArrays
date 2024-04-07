599a797a9fde5df1db4162e1a650cc1660012c46
Harosshu <samuil.batsinski@gmail.com>
2024-04-07 23:08:30 +0100

    SringsAndArrays

---
+++ RandomElement/Program.cs
@@ -0,0 +1,15 @@
+﻿//string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+//Random rnd = new Random();
+
+//int randomIndex = rnd.Next(0, snacks.Length);
+
+//Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, colors.Length);
+string randomColor = colors[randomIndex];
+
+Console.WriteLine($"You should wear {randomColor} today.");
